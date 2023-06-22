b2b7b8014f1266d6d8765546de77b63a94a4af1a
504 <504@DESKTOP-FRGIVND>
2023-06-22 18:11:28 +0900

    반복문 별찍기, 회원가입 로그인 페이지 구현 과제

--- frontend/public/과제/JavaScript과제/JS과제_2(회원가입 및 로그인).js
+++ frontend/public/과제/JavaScript과제/JS과제_2(회원가입 및 로그인).js
@@ -1,6 +1,6 @@
 /* 비어 있는 배열 선언 */
-let id = []
-let pw = []
+let idlist = []
+let pwlist = []
 
 /* 회원가입 함수 */
 function 회원가입(){
@@ -13,20 +13,19 @@ function 회원가입(){
 	let pw0 = pw_input.value
 	
 	/* 유효성 검사*/
-	if (id.indexOf(id0) == -1 ) {
-		id.push (id0)
-		pw.push (pw0)
-		console.log ('회원가입 되었습니다.') 
-		alert('회원가입 되었습니다.');
-	} 
-	else {
-		console.log (' 이미 존재하는 아이디 입니다.') 
-		alert (' 이미 존재하는 아이디 입니다.');
-	}
+	if (idlist.indexOf(id0) == -1 ) {
+		if (id_input.value.length >=8){
+			if(pw_input.value.length >=8){
+				idlist.push(id0); pwlist.push(pw0);
+				alert('회원가입 되었습니다.')
+			}else {alert('비밀번호를 8글자 이상 입력해주세요.')}
+		}else {alert('아이디를 8글자 이상 입력해주세요.')}
+	}else{alert('중복된 아이디입니다.')}
+
 
 	/* 콘솔창에 출력 */
-	console.log(id)
-	console.log(pw)
+	console.log(idlist)
+	console.log(pwlist)
 	
 	/* 데이터 저장 이후 <input> 객체안의 입력값 초기화 */
 	id_input.value = ''
@@ -36,16 +35,16 @@ function 회원가입(){
 /* 로그인 함수*/
 function 로그인(){
 	/* 입력값 받아오기 */
-	let id_input = document.querySelector(".id")
-	let pw_input = document.querySelector(".pw")
+	let id_input = document.querySelector(".id_1")
+	let pw_input = document.querySelector(".pw_1")
 	
 	/* 입력받은 데이터를 저장하는 과정 */
 	let id0 = id_input.value
 	let pw0 = pw_input.value
 	
 	/* 유효성 검사*/
-	if (id.indexOf(id0) != -1 ){
-		if (pw.indexOf (pw0 != -1)){
+	if (idlist.indexOf(id0) != -1 ){
+		if (pwlist.indexOf (pw0 != -1)){
 		console.log (' 로그인 성공.') 
 		alert (' 로그인 성공.');
 		}
@@ -56,8 +55,8 @@ function 로그인(){
 	
 	
 	/* 콘솔창에 출력 */
-	console.log(id)
-	console.log(pw)
+	console.log(id_input)
+	console.log(pw_input)
 	
 	/* 데이터 저장 이후 <input> 객체안의 입력값 초기화 */
 	id_input.value = ''
