9d06eaa699099df2c447e22ed27fbe3210deb4a0
504 <504@DESKTOP-OM2NF7O>
2023-07-03 15:07:43 +0900

    JS과제_5

--- frontend/public/과제/3. JavaScript과제/JS과제_5/JS과제_5.js
+++ frontend/public/과제/3. JavaScript과제/JS과제_5/JS과제_5.js
@@ -1,3 +1,23 @@
-/**
- * 
+/*
  */
+
+ 
+ //1. 조작, 제어할 HTML 태그[DOM객체]를 호출
+ let userbox = document.querySelector('.userbox');
+ // 어떻게 : 현재 페이지(전체)에서 키보드를 누를때 
+ 	// DOM객체명.addEventListener('이벤트명', ()=>{})
+ document.body.addEventListener('keydown', (e)=>{
+	 console.log('키눌림')
+	 //1. 눌린 키 상태 확인 .keyCode
+	 console.log(e)
+	 
+	 if( e.keyCode == 37 ){
+		 console.log("왼쪽으로 이동"); // css left 차감
+		 userbox.style.left--;
+	 }else if ( e.keyCode == 39 ){
+		 console.log ("오른쪽으로 이동"); //css left 증가
+		 userbox.style.left++;
+	 }
+	 console.log (userbox.style.left);
+ })
+ 
