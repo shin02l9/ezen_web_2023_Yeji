591ac2383675fb75afbbd553b48af1de61e67242
504 <504@DESKTOP-OM2NF7O>
2023-07-13 16:51:23 +0900

    과제 제출

---
+++ backend/src/과제/과제01/과제01.java
@@ -0,0 +1,52 @@
+package 과제.과제01;
+
+public class 과제01 { // class s
+	public static void main(String[] args) { // main s
+		
+		
+		// 연산자 암기 확인 (2인 1조)
+		// p.73~76 풀이
+		
+		/*
+		 Q.1
+		 A: 4번
+		 
+		 Q.2
+		 A: 3,4,5,6,7번
+		 
+		 Q.3
+		 A: 			1byte       2byte       4byte       8byte
+		 -----------------------------------------------------------------
+		 	정수타입 :     byte     short,char   int(기본형)    long
+		 	실수타입 : 						    float       double
+		 	논리타입 :    boolean
+		 
+		 Q.4
+		 A: 타입  : int, double
+		 	변수  : age, price
+		 	리터럴 : 10, 3.14
+		 
+		 Q.5
+		 A: 2번
+		 
+		 Q.6
+		 A: 4번
+		 
+		 Q.7
+		 A: 3번
+		 
+		 Q.8
+		 A: 3번
+		 
+		 Q.9
+		 A: 2번
+		 
+		 Q.10
+		 A: 2번
+
+		*/
+		
+		
+		
+	} // main e
+} // class e
