150e8cefa3b0a70c0dd3ae78fb1926afd20b5fc6
504 <504@DESKTOP-OM2NF7O>
2023-07-24 09:29:03 +0900

    과제 6

--- backend/src/과제/과제06/과제6_3_키오스크파일처리.java
+++ backend/src/과제/과제06/과제6_3_키오스크파일처리.java
@@ -25,12 +25,12 @@ public class 과제6_3_키오스크파일처리 { // class s
 			// ------파일내 제품정보 문자열 ---> 배열에 저장 -----------------------------------------------------
 				// 1. 하나의 문자열로 모든 제품의 문자열을 각 제품별로 분리
 				String[] 재고관리 = fileInfo.split("\n"); 
-				System.out.println("제품분리 :" + (Arrays.toString(재고관리)) );
+				//System.out.println("제품분리 :" + (Arrays.toString(재고관리)) );
 
 			
 			// ------ 키오스크 프로그램 실행-----------------------------------------------------
 			System.out.println("\n\n-------------------- 메뉴 ------------------- ");
-			System.out.print("-1: 제품등록 0: 결제 \n");
+			System.out.print("-1: 제품등록 | 0: 결제 \n");
 				//재고관리 배열에 있는 제품만 선택가능하도록 출력
 				for( int i=0; i<재고관리.length; i++) { // for s
 					String product = 재고관리[i];
@@ -47,7 +47,7 @@ public class 과제6_3_키오스크파일처리 { // class s
 					} // for e
 			System.out.print(">>>>>> 선택 : "); int ch = scanner.nextInt();
 			
-			// ------ 제품등록하기 ------
+			// ------------------------ 제품 등록하기 ------------------------
 			if ( ch == -1 ) { // 만약에 -1번을 입력하면 제품등록 가능
 				//1. 추가할 제품의 정보를 입력받아 각 변수에 저장
 				System.out.print("제품명 : "); String name = scanner.next();
@@ -62,47 +62,39 @@ public class 과제6_3_키오스크파일처리 { // class s
 				fileOutputStream.write( outstr.getBytes() ); //2,3단계 1단계는 위에 !
 
 				System.out.println("안내) 제품이 등록되었습니다.");
-			}
+			} 
+			// ------------------------ 제품 주문하기 ------------------------
+			else if ( ch > 0 && ch <= 재고관리.length ) {
+				// 1. 선택한 제품번호의 각 정보 호출 : ch-1 : 선택한 제품의 인덱스 
+				String name = (재고관리[ch-1].split(",")[0]); // 제품명
+				int stock = Integer.parseInt(재고관리[ch-1].split(",")[1]); // 재고
+				int price = Integer.parseInt(재고관리[ch-1].split(",")[2]); // 가격
+				int basket = Integer.parseInt(재고관리[ch-1].split(",")[3]); // 바구니
+				
+				// 2. 재고가 0보다 크면 재고/바구니 증감, 재고없으면 안내만하고 행동x
+				if( stock > 0 ) {	stock--; basket++; System.out.println( name + "제품을 담았습니다.");
+				} else { System.out.println( "재고가 부족합니다."); }
+				
+				// 3. 배열의 상태 업데이트 하기
+				재고관리[ch-1] = name+","+stock+","+price+","+basket;
+				
+				// 4. 파일 내보내기 또해야함.. // 파일내 제품들의 변화가 있으므로 업데이트
+				// 현재 재고관리배열의 상태를 파일에 저장한다.
+				String outstr = "";
+				for ( int i = 0; i < 재고관리.length; i++) { // 배열내 모든 데이터를 하나의 문자열에 변환
+					outstr += 재고관리[i].split(",")[0]+","+재고관리[i].split(",")[1]+","+재고관리[i].split(",")[2]+","+재고관리[i].split(",")[3]+"\n";
+				}
+				// 5. 재고관리 배열을 하나의 문자열로 변환된 문자열을 바이트배열로 변환 후 내보내기 한다.
+				// 이어쓰기가 아니라 덮어쓰기를 해야한다.
+				FileOutputStream fileOutputStream2 = new FileOutputStream(filePath);
+				fileOutputStream2.write( outstr.getBytes() );
 			
+			// ------------------------ 결제하기 ------------------------
+			} else if ( ch == 0 ) {
+
+			}
 		
 		} // while e
 	} // main e
 } // class e
 
-
-//// 주문 선택 
-//if ( ch == 1 || ch == 2 || ch == 3 ) {
-//	//배열 호출
-//	//1. 배열변수명호출	
-//	// System.out.println("배열의 변수명 호출 : "+ 재고관리 );
-//	// System.out.println("배열의 데이터 호출 : "+ Arrays.toString(재고관리));
-//	// System.out.println("배열의 데이터 개수 : "+ 재고관리.length);
-//	// System.out.println("배열내 특정 데이터[인덱스] 호출 : "+ 재고관리[0]);
-//	String 선택된제품정보 = 재고관리[ch-1];
-//	//2. 배열내 데이터{문자열) 분리
-//	int 선택된제품재고 = Integer.parseInt(선택된제품정보.split(",")[0]);
-//	int 선택된제품바구니 = Integer.parseInt(선택된제품정보.split(",")[1]);
-//	int 선택된제품가격 = Integer.parseInt(선택된제품정보.split(",")[2]);
-//	String 선택된제품이름 = 재고관리[ch-1].split(",")[3];
-//	//3. 유효성 검사를 통한 상태 변경
-//	if( 선택된제품재고 > 0 ) {
-//		선택된제품재고--; 선택된제품바구니++; System.out.println(선택된제품이름+"장바구니에 담았습니다.");
-//	} else {System.out.println("재고가 부족합니다.");}
-//	//4. 재고관리 업데이트
-//	재고관리[ch-1] = 선택된제품재고+","+선택된제품바구니+","+선택된제품가격;
-//
-//} else if ( ch == 0 ) {
-//	System.out.println(Arrays.toString(재고관리)); 
-//	System.out.printf( "%10s %10s \n", "수량","가격");
-//	
-//	
-//	for( int i =0; i< 재고관리.length; i++) {
-//		int basket = Integer.parseInt( 재고관리[i].split(",")[1] );
-//		int price = Integer.parseInt( 재고관리[i].split(",")[2] );
-//		String name = 재고관리[i].split(",")[3];
-//		if( basket > 0 ) {
-//			System.out.printf( "%10s %10s %10s \n ", name, basket, basket*price);
-//			System.out.printf( "%10s %10s %10s \n ", name, basket, basket*price);
-//		}
-//	}
-//}
