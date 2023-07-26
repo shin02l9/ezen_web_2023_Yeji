a3b3ad2e03596a2ad25898dccae7113a88c6492c
504 <504@DESKTOP-OM2NF7O>
2023-07-26 16:18:13 +0900

    객체과제

--- backend/src/과제/객체과제4인/파티.java
+++ backend/src/과제/객체과제4인/파티.java
@@ -16,7 +16,7 @@ public class 파티 {
 				System.out.println("파티가입이되었습니다");
 				if(i==파티1.length){System.out.println("파티원모집이 완료되었습니다");}
 				break;
-			} 
+			}  
 		}if(count==0){System.out.println("파티원모집이 완료되었습니다");}
 	}
 	//3. 메소드
