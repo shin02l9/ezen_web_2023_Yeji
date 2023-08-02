80437e839d0abce2665cf032ff3d6595ded783d4
504 <504@DESKTOP-QK2R2S4>
2023-08-02 16:47:54 +0900

    SQL 기초

--- backend/src/database/01_SQL기초.sql
+++ backend/src/database/01_SQL기초.sql
@@ -82,32 +82,32 @@
             
 		- 데이터 필드 타입 [ myslq기준 *DBMS 조금씩 다름 ]
 			1. 정수
-				tinyint(1)	: -128 ~ 127
-                smallint(2) : +- 3만정도
-                mediumint(3): +- 8백만정도
-                int(4)		: +- 21억정도
-                bigint(8) 	: 21억 이상
+				tinyint		[1]	: -128 ~ 127
+                smallint	[2] : +- 3만정도
+                mediumint	[3]	: +- 8백만정도
+                int			[4]	: +- 21억정도
+                bigint		[8] : 21억 이상
 			2. 실수
-				float(4)	: 소수점 7자리
-                double(8)	: 소수점 15자리
+				float		[4]	: 소수점 7자리
+                double		[8]	: 소수점 15자리
             3. 문자
-				<글자수 최소1 ~ 최대255>
+				[글자수 최소1 ~ 최대255]
 				char(문자길이)		: 고정길이	  char(3)-> 'ad' -> 3바이트 	 [*저장할 데이터의 글자수가 정확히 정해져 있는 경우]
-                <글자수 최소1 ~ 최대65535>
+                [글자수 최소1 ~ 최대65535]
                 varchar(문자길이)	: 가변길이	  varchar(3)-> 'ad' -> 2바이트 [*저장할 데이터의 글자수가 정확하지 않는 경우]
 			4. 대용량 문자
-				<글자수 최소1 ~ 최대65535>
+				[글자수 최소1 ~ 최대65535]
 				text
-                <글자수 최소1 ~ 최대16000000>
+                [글자수 최소1 ~ 최대16000000]
                 mediumtext
-                <최대 글자수 억대 넘어감 4G 정도>
+                [최대 글자수 억대 넘어감 4G 정도]
                 longtext		: 게시물의 내용물, 첨부파일, 이미지, 동영상 등등
             5. 날짜
-				date(날짜)		: YYYY-MM-DD
-                time(시간)		: HH:MM:SS
-                datetime(날짜시간) : YYYY-MM-DD HH:MM:SS
+				date			: (날짜) YYYY-MM-DD
+                time			: (시간) HH:MM:SS
+                datetime 		: (날짜시간) YYYY-MM-DD HH:MM:SS
             6. 논리
-				boolean(1)		: 0 or 1
+				boolean[1]		: 0 or 1
 */
 
 		#예1 : 데이터베이스(여러개의 테이블(표)들을 저장 할수 있는 공간) 생성
@@ -141,7 +141,7 @@
 
 		#예5 : 만약에 데이터베이스 없을떄 삭제 명령어 사용하면 오류 발생
         drop database if exists sqldb1;
-        drop database if exists sqldb2;
+        drop database if exists membersqldb2;
 	
 		#예6 : 여러개의 데이터베이스중 사용할 데이터 베이스 선택 !!
         use sqldb1;
@@ -173,13 +173,163 @@
         #예2: 테이블 삭제
         drop table member;
         
-        
+        #활용2 : 
+			/*
+			문제 1:
+				1.웹개발을 하는데 DB저장소가 필요해 'sqldb1web' 이름으로 DB 생성
+                2. 해당 DB에 member 테이블 생성
+					id (문자열 최대 20)
+                    pw (문자열 최대 10)
+			*/
+            #1. 만약에 생성할 이름의 DB명이 존재하면 삭제하기
+            drop database if exists sqldb1web;
+            #2. DB 생성
+            create database sqldb1web;
+            #3. DB 사용 선택
+            use sqldb1web;
+            #4. 만약에 생성할 이름의 테이블명이 존재하면 삭제하기
+			drop database if exists member;
+            #5. 테이블 생성하기
+            create table member(id varchar(20), pw varchar(10));
+            
+			/*
+			문제 2:
+				1.웹개발을 하는데 DB저장소가 필요해 'sqldb1web2' 이름으로 DB 생성
+                2. 해당 DB에 board 테이블 생성
+					게시물번호 		(필드명 : bno 			타입 : 최대 21억 정도)
+					게시물제목 		(필드명 : btitle 		타입 : 문자열 최대 100)
+                    게시물내용 		(필드명 : bcontent  	타입 : 문자열 최대 6만 5천자 이상)
+                    게시물조회수 	(필드명 : bview 		타입 : 최대 21억 정도)
+                    게시물작성일 	(필드명 : bdate 	타입 : 날짜/시간)
+			*/
+			#1. 만약에 생성할 이름의 DB명이 존재하면 삭제하기
+            drop database if exists sqldb1web2;
+            #2. DB 생성
+            create database sqldb1web2;
+            #3. DB 사용 선택
+            use sqldb1web2;
+            #4. 만약에 생성할 이름의 테이블명이 존재하면 삭제하기
+			drop database if exists board;
+            #5. 테이블 생성하기
+            create table board(bno int, btitle varchar(100), bcontent longtext, bview int, bdate datetime);
+            select *from board; -- 만든 테이블 확인하기~
+            
+            
+            /*
+				문제3 : 
+					1. 웹개발 하는데 DB저장소 'sqldb2web1' 이름으로 DB 생성
+					2. 해당 db에 memberboard 테이블 생성
+							회원번호		( 필드명 : mno		타입 : 최대 21억정도 )
+			                회원아이디		( 필드명 : mid		타입 : 문자열 최대 20 )
+			                회원비밀번호 	( 필드명 : mpw		타입 : 문자열 최대 20 )
+							게시물번호 	( 필드명 : bno  		타입 : 최대 21억정도 )
+							게시물제목		( 필드명 : btitle 	타입 : 문자열 최대 100 )	
+							게시물내용 	( 필드명 : bcontent 	타입 : 문자열 최대 6만5천 이상 )
+							게시물조회수	( 필드명 : bview 		타입 : 최대 21억정도 )
+							게시물작성일 	( 필드명 : bdate 		타입 : 날짜/시간 )
+			*/
+			# 1. 데이터베이스 생성 
+			drop database if exists sqldb2web1; # 만약에 동일한 db명이 존재했을때 생성불가능 하기 때문에 만약에 존재하면 삭제
+			create database sqldb2web1; 		# 실행 : [명령어]; 마다 컨트롤+엔터 
+			# 2. 테이블 생성 
+			use sqldb2web1;
+			drop table if exists memberboard;
+			create table memberboard(
+					mno int , 			
+			        mid varchar(20)	, 		
+			        mpw varchar(20) , 		
+					bno int ,				
+					btitle varchar(100) , 	
+					bcontent longtext , 	
+					bview int , 			
+					bdate datetime 		
+			);
+			select * from memberboard;
+			
+			/*
+				문제4 : 
+					1. 웹개발 하는데 DB저장소 'sqldb2web1' 이름으로 DB 선택
+					2. 해당 db에 memberboard 테이블을 중복을 최소화 할수 있도록 2개의 테이블로 분리하여 각각 생성 [ 필드과 타입은 문제3 동일 ]
+			*/
+			
+			use sqldb2web1;
+			drop table if exists member;
+			create table member(
+					mno int , 				
+			        mid varchar(20)	, 	
+			        mpw varchar(20)  	
+			);
+			select * from member;
+			
+			drop table if exists board;
+			create table board(
+					bno int ,				
+					btitle varchar(100) , 
+					bcontent longtext , 	
+					bview int , 			
+					bdate datetime 		
+			);
+			select * from board;
+			# ----------------------------> 테이블 관계 상태 확인 : 메뉴 -> database -> reverse enginner
+			
+			-- 예1 : 1:M 관계 / PK:FK 관계 
+			use sqldb2web1;
+			drop table if exists member2;
+			create table member2(
+					mno_pk int , 				
+			        mid varchar(20)	, 		
+			        mpw varchar(20) ,
+					primary key( mno_pk )	-- 현재 테이블에서 mno_pk 라는 필드를 식별키로 사용 
+			);
+			drop table if exists board2;
+			create table board2(
+					bno int ,				
+					btitle varchar(100) , 	
+					bcontent longtext , 	
+					bview int , 			
+					bdate datetime ,
+			        mno_fk int ,  			-- 1. FK 필드 선언 
+			        foreign key( mno_fk ) references member2( mno_pk ) 	-- 2. 현재 테이블에서 mno_fk 라는 필드를 외래키로 사용 
+						# foreign key( mno_fk ) 		: 'mno_fk' 라는 필드를 FK 필드로 설정 
+			            # references member2( mno_pk ) 	: FK필드를 member2 테이블의 mno_pk 필드와 참조.. [ 관계 ] 
+			);
+
+
+/*
+ 
+		 * PK 	: 식별키
+		 * FK	: 참조키 (?)
+ 
+
+	PK와 FK 필드 선정 
+		PK : 데이터의 중복이 없는 식별 가능한 필드 [ 후보 : mno, mid ]
+			- 번호 형태의 PK 권장하는 이유 : 문자열 데이터 필드보다 메모리 효율적으로 사용가능.
+			테이블1개당 PK필드 1개 이상 권장 O
+			
+		FK : 다른 테이블에 있는 PK필드와 연결
+			1. FK필드명은 PK필드명과 동일하게 사용 
+			2. [필수] FK필드의 타입은 PK필드의 타입과 동일
+			3. [참조하는 테이블]에서 FK필드 
 
 
 
 
 
-
-
-
-
+*/
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
